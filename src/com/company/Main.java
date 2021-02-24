84c0874c890612c934e72532974b912b20035993
sergeystolyarov86 <sergey_stolyarov@mail.ru>
2021-02-24 15:52:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,118 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    } // Проверка победы циклами,какие то мысли пришлось поискать в интернете,на дополнительные задания пока нет времени
+      // но обязательно найду время позже
+    public static boolean checkWin(char symb) {
+        int diag1 = 0;
+        int diag2 = 0;
+        for (int i = 0; i < SIZE; i++) {
+            diag1 += map[i][i];
+            diag2 += map[i][SIZE - (i + 1)];
+        }
+        if (diag1==symb*3 || diag2==symb*3) {
+            return true;
+        }int check_i;
+        int check_j;
+        for(int i=0;i<SIZE;i++) {
+            check_i = 0;
+            check_j = 0;
+            for (int j = 0; j < SIZE; j++) {
+                check_i += map[i][j];
+                check_j += map[j][i];
+            } if(check_i==symb*SIZE || check_j==symb*SIZE){return true;}
+            }
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
